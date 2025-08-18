66146675b57138984bc80c203f3cda59dd2bed38
ChococupC <Qiaokebei123@gmail.com>
2025-08-17 20:54:46 -0400

    changed fetch

--- src/tools/Connect.jsx
+++ src/tools/Connect.jsx
@@ -14,9 +14,20 @@
 //   },
 //   status: "success",
 // };
+
+function getLocalDate() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // months are 0-based
+  const day = String(now.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
 async function getCategories() {
+  const dt = getLocalDate();
+  console.log(dt);
   const res = await fetch(
-    "https://samoyes.onrender.com/samoyes_API/categorize/"
+    `https://samoyes.onrender.com/samoyes_API/categorize/?date=${dt}`
   );
   const data = await res.json();
   return data;
